0c4161de230849e0d25f2f4a8f4ae17963ef48c9
LeonardoCahuichMX <leonardo.cahuich@icloud.com>
2025-05-17 22:30:20 -0600

    Se mejora inmutabilidad bloqueando 'nix-env' y 'nix-shell' por parte de usuarios y root. (Se repara error)

--- flake.nix
+++ flake.nix
@@ -4,52 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
-  };
-
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    {
-      nixosConfigurations.thinkpad = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./hardware-configuration/thinkpad-hw.nix
-          ./hosts/thinkpad.nix
-        ];
-        specialArgs = { inherit self; };
-        # 👇 Importar los overlays desde el archivo
-        pkgs = import nixpkgs {
-          system = "x86_64-linux";
-          overlays = import ./overlays; # <- Aquí se importa la lista
-          config.allowUnfree = true;
-        };
-      };
-
-      packages.x86_64-linux.deploy-thinkpad =
-        let
-          pkgs = import nixpkgs {
-            system = "x86_64-linux";
-            config.allowUnfree = true;
-            overlays = import ./overlays;
-          };
-        in
-        pkgs.writeShellScriptBin "deploy-thinkpad" ''
-          echo "⚙️ Ejecutando despliegue para ThinkPad..."
-          nix flake lock
-          sudo nixos-rebuild switch --flake .#thinkpad
-        '';
-    };
-}
-
-/*
-{
-  description = "Sistema NixOS inmutable para ThinkPad L15";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    flake-utils.url = "github:numtide/flake-utils";
     #impermanence.url = "github:nix-community/impermanence";
   };
 
-  outputs = { self, nixpkgs, flake-utils, impermanence, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     {
       nixosConfigurations.thinkpad = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -74,4 +32,4 @@
           sudo nixos-rebuild switch --flake .#thinkpad
         '';
     };
-}*/
+}
